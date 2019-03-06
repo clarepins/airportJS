d7bc4618af578d7093509e16f8ead19808cf41aa
Clare Pinder <Clare.pinder@live.co.uk>
2019-03-06 15:37:19 +0000

    simple airport with random weather and cannot land if stormy

--- jasmine-standalone-3.3.0/spec/airportSpec.js
+++ jasmine-standalone-3.3.0/spec/airportSpec.js
@@ -3,6 +3,7 @@ describe('Airport', function() {
 beforeEach(function() {
   airport = new Airport();
   planeSpy = jasmine.createSpy('plane');
+  planeSpy2 = jasmine.createSpy('plane');
 });
 
   it('can land a plane', function() {
@@ -14,6 +15,22 @@ beforeEach(function() {
     airport.land(planeSpy);
     airport.takeOff(planeSpy);
     expect(airport.planes).not.toContain(planeSpy);
+  });
+
+  it('throws an error if full', function() {
+      airport.land(planeSpy);
+      expect(function() {airport.land(planeSpy2)}).toThrow('Cannot land.');
+  });
+
+  it('can land if clear', function() {
+    spyOn(airport, "_stormy").and.returnValue(false)
+    airport.land(planeSpy);
+    expect(airport.planes).toContain(planeSpy);
+  });
+
+  it('cannot land if stormy', function() {
+    spyOn(airport, "_stormy").and.returnValue(true)
+    expect(function() {airport.land(planeSpy)}).toThrow('Cannot land.');
   });
 
 });
--- jasmine-standalone-3.3.0/src/airport.js
+++ jasmine-standalone-3.3.0/src/airport.js
@@ -3,10 +3,23 @@ function Airport() {
 }
 
 Airport.prototype.land = function(plane) {
-  this.planes.push(plane)
+  if (this.planes.length >= 1 || this._stormy()) {
+    throw('Cannot land.');
+  } else {
+  this.planes.push(plane);
+  }
 };
 
 Airport.prototype.takeOff = function(plane) {
-  i = this.planes.indexOf(plane)
+  var i = this.planes.indexOf(plane)
   this.planes.splice(i, 1);
+};
+
+Airport.prototype._stormy = function() {
+  var j = Math.floor(Math.random())
+  if (j > 0.8 ) {
+    return true;
+  } else {
+    return false;
+  };
 };
