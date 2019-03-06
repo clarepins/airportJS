8ff2b38ad167ba9345382351f1b515429799da53
Clare Pinder <Clare.pinder@live.co.uk>
2019-03-06 15:01:04 +0000

    can take off a plane

--- jasmine-standalone-3.3.0/spec/airportSpec.js
+++ jasmine-standalone-3.3.0/spec/airportSpec.js
@@ -1,10 +1,19 @@
 describe('Airport', function() {
 
-planeSpy = jasmine.createSpy('plane');
+beforeEach(function() {
+  airport = new Airport();
+  planeSpy = jasmine.createSpy('plane');
+});
 
   it('can land a plane', function() {
-    airport = new Airport();
     airport.land(planeSpy);
     expect(airport.planes).toContain(planeSpy);
   });
+
+  it('can take off a plane', function() {
+    airport.land(planeSpy);
+    airport.takeOff(planeSpy);
+    expect(airport.planes).not.toContain(planeSpy);
+  });
+
 });
--- jasmine-standalone-3.3.0/src/airport.js
+++ jasmine-standalone-3.3.0/src/airport.js
@@ -5,3 +5,8 @@ function Airport() {
 Airport.prototype.land = function(plane) {
   this.planes.push(plane)
 };
+
+Airport.prototype.takeOff = function(plane) {
+  i = this.planes.indexOf(plane)
+  this.planes.splice(i, 1);
+};
